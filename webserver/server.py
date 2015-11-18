948eb9ef73220e4e470d83f0748dd1dc586395b3
Ubuntu <azureuser@lzhan1.lzhan1.c1.internal.cloudapp.net>
2015-11-18 00:21:44 +0000

    recipe page

--- webserver/server.py
+++ webserver/server.py
@@ -120,6 +120,19 @@ def index():
   #
   return render_template("index.html", **context)
 
+@app.route('/recipePage/<r_id>', methods = ["POST", "GET"])
+def recipePage(r_id):
+  result = []
+  cursor = g.conn.execute("SELECT * FROM recipes WHERE name == :n ;", n = r_id)
+  result = cursor.fetchone()
+  #r_instr = result['instructions'] 
+  #r_time = result['perparation_time']
+  #r_user = result['user_posted']
+  
+  cursor.close()
+  #, r_instr = r_instr, r_time=r_time, r_user = r_user
+  return render_template("recipePage.html", r_id=r_id)
+
 @app.route('/test/', methods =["POST", "GET"])
 def test():
   return render_template("test.html")
